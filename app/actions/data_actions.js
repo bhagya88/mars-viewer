93e08ca9cb5a3f560287cfbca9c83099630836f2
bhagya <bhagya@outlook.com>
2017-02-25 09:42:12 -0600

    checking on heroku

--- app/actions/data_actions.js
+++ app/actions/data_actions.js
@@ -33,39 +33,40 @@ export function  getImgURL(date){
 		const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date='+date+'&api_key='+apiKey;
 
 
-    var myHeaders = new Headers({
+	    var myHeaders = new Headers({
 
-    });
+	    });
 
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Access-Control-Allow-Credentials", "false");
-    myHeaders.append("Access-Control-Allow-Origin", "*");
-    myHeaders.append("Access-Control-Allow-Headers", "*");
-    myHeaders.append("Access-Control-Allow-Methods", "*");
+	    myHeaders.append("Content-Type", "application/json");
+	    myHeaders.append("Access-Control-Allow-Credentials", "false");
+	    myHeaders.append("Access-Control-Allow-Origin", "*");
+	    myHeaders.append("Access-Control-Allow-Headers", "*");
+	    myHeaders.append("Access-Control-Allow-Methods", "*");
 
 
-	var myInit = { method: 'GET',
-               headers: myHeaders,
-               mode: 'no-cors',
-               cache: 'default' ,
-               json: true,
-               credentials: 'omit'
-           };
+		var myInit = { 
 
-    var request = new Request(url, myInit);
+			       method: 'GET',
+	               headers: myHeaders,
+	               mode: 'no-cors',
+	               cache: 'default' ,
+	               credentials: 'omit'
+	           };
+
+	    var request = new Request(url, myInit);
     
-	fetch(request)
+		fetch(request)
 
-		.then(data=>data.json())
-		.then(data=>{
-			
-			if(data.photos && data.photos.length){
-				console.log(data.photos[0].img_src);
-				despatch(setImgURL(data.photos[0].img_src));
-			}else{
-				despatch(setImgFound('No'));
-			}
-		});
-	}
+			//.then(data=>data.json())
+			.then(data=>{
+				
+				if(data.photos && data.photos.length){
+					console.log(data.photos[0].img_src);
+					despatch(setImgURL(data.photos[0].img_src));
+				}else{
+					despatch(setImgFound('No'));
+				}
+			});
+		}
 }
 
